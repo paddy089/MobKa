0a25c0f0f253c8821967b8796c73bfa2306ea254
Patrick Ring <ringpatrick89@gmail.com>
2017-05-17 22:49:09 +0200

    Change colors of action bar and bottom bar on view change.

--- app/src/main/java/com/example/paddy/mobka/MainActivity.java
+++ app/src/main/java/com/example/paddy/mobka/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -25,14 +26,17 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_karte:
                     mTextMessage.setText(R.string.title_karte);
                     changeColors(R.color.colorMapViewPrimary, R.color.colorMapViewSecondary);
+                    //setContentView(R.layout.map_view);
                     return true;
                 case R.id.navigation_favoriten:
                     mTextMessage.setText(R.string.title_favoriten);
                     changeColors(R.color.colorfavoritesViewPrimary, R.color.colorfavoritesViewSecondary);
+                    //setContentView(R.layout.map_view);
                     return true;
                 case R.id.navigation_optionen:
                     mTextMessage.setText(R.string.title_optionen);
                     changeColors(R.color.coloroptionsViewPrimary, R.color.coloroptionsViewSecondary);
+                    //setContentView(R.layout.favorites_view);
                     return true;
             }
             return false;
@@ -47,8 +51,16 @@ public class MainActivity extends AppCompatActivity {
         changeColors(R.color.colorListViewPrimary, R.color.colorListViewSecondary);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setTitle(R.string.title_favoriten);
+
+
+
     }
 
     private void changeColors(int primaryColor, int secondaryColor){
@@ -56,6 +68,13 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setNavigationBarColor(ContextCompat.getColor(getApplicationContext(), secondaryColor));
         getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), secondaryColor));
+        //navigation.setBackgroundColor(primaryColor);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), primaryColor));
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), primaryColor));
 
     }
 
