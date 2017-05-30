049abb04ac4b64bca425d1424b5a4922c4a1c8dc
Patrick Ring <ringpatrick89@gmail.com>
2017-05-30 10:27:27 +0200

    New bottom navigation.

--- app/src/main/java/com/example/paddy/mobka/MainActivity.java
+++ app/src/main/java/com/example/paddy/mobka/MainActivity.java
@@ -5,13 +5,10 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,24 +16,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_liste:
-                    mTextMessage.setText(R.string.title_liste);
-                    changeColors(R.color.colorListViewPrimary, R.color.colorListViewSecondary);
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    //changeColors(R.color.colorListViewPrimary, R.color.colorListViewSecondary);
+
+
                     return true;
-                case R.id.navigation_karte:
-                    mTextMessage.setText(R.string.title_karte);
-                    changeColors(R.color.colorMapViewPrimary, R.color.colorMapViewSecondary);
-                    //setContentView(R.layout.map_view);
+                case R.id.navigation_map:
+                    //mTextMessage.setText(R.string.title_map);
+                    //changeColors(R.color.colorMapViewPrimary, R.color.colorMapViewSecondary);
+                    //item.setIcon(R.drawable.ic_nav_home_active);
                     return true;
-                case R.id.navigation_favoriten:
-                    mTextMessage.setText(R.string.title_favoriten);
-                    changeColors(R.color.colorfavoritesViewPrimary, R.color.colorfavoritesViewSecondary);
-                    //setContentView(R.layout.map_view);
-                    return true;
-                case R.id.navigation_optionen:
-                    mTextMessage.setText(R.string.title_optionen);
-                    changeColors(R.color.coloroptionsViewPrimary, R.color.coloroptionsViewSecondary);
-                    //setContentView(R.layout.favorites_view);
+                case R.id.navigation_more:
+                    //mTextMessage.setText(R.string.title_more);
+                    //changeColors(R.color.coloroptionsViewPrimary, R.color.coloroptionsViewSecondary);
+                    //item.setIcon(R.drawable.ic_nav_home_active);
                     return true;
             }
             return false;
@@ -48,22 +42,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeColors(R.color.colorListViewPrimary, R.color.colorListViewSecondary);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        myToolbar.setTitle(R.string.title_favoriten);
 
 
+        //Window window = activity.getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        //getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.colorWhite));
 
     }
 
-    private void changeColors(int primaryColor, int secondaryColor){
+
+
+    /*private void changeColors(int primaryColor, int secondaryColor){
         //getWindow().setNavigationBarColor(ContextCompat.getColor(getApplicationContext(), R.color.colorMapViewSecondary));
 
         getWindow().setNavigationBarColor(ContextCompat.getColor(getApplicationContext(), secondaryColor));
@@ -76,6 +79,6 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), primaryColor));
 
-    }
+    }*/
 
 }
